82a84e05b9a3d2f0861becb2d1fe23f18d0fffb4
EXTCAH-rickweil <rick.weil@sparton.com>
2016-11-09 12:33:07 -0500

    ascii/binary with interrupts

--- SPI-slave/SPI-slave.ino
+++ SPI-slave/SPI-slave.ino
@@ -3,7 +3,9 @@
 char tx [100];
 char rx [100];
 volatile byte pos;
-volatile boolean process_it;
+volatile boolean msg_ready;
+bool is_ascii = false;
+int messages = 0;
 
 void setup (void)
 {
@@ -17,39 +19,58 @@ void setup (void)
   
   // get ready for an interrupt 
   pos = 0;   // txfer empty
-  process_it = false;
+  msg_ready = false;
 
   // now turn on interrupts
   SPI.attachInterrupt();
-
+  attachInterrupt(digitalPinToInterrupt(21), eotISR, RISING);
 }  // end of setup
+
+void eotISR() {
+    messages++;
+    msg_ready = true;
+}
 
 
 // SPI interrupt routine
 ISR (SPI_STC_vect) {
 
-  byte rd = SPDR;     // grab byte from SPI Data Register
-  char wr;
-  wr = toupper(rd);// +(rd != 0);  // create modified write data from read (e.g. increment)
- 
-  if (pos < sizeof(rx)) {
-    tx[pos] = wr;     // tx_buffer with modified data
-    rx[pos++] = rd;   // add to rx_buffer
-  }
-  SPDR = wr;
-  if (rd == 0)        // NUL means time to process received data
-    process_it = true;   
+    byte rd = SPDR;     // grab byte from SPI Data Register
+    // echo back, but if ASCII, toupper(c)
+    char wr = is_ascii ? toupper(rd) : rd;
+
+    if (pos < sizeof(rx)) {
+        tx[pos] = wr;     // tx_buffer with modified data
+        rx[pos++] = rd;   // add to rx_buffer
+    }
+
+    SPDR = wr;
+    if ((rd == 0 && is_ascii))       //ASCII NUL terminator
+        msg_ready = true;   
 }
 
+unsigned long timer=0;
 void loop (void) 
 {
-// wait for flag set in interrupt routine
-  if (process_it) {
-    tx [pos] = 0;  // make sure we're nul terminated
-    rx [pos] = 0;  
-    Serial.print(  String("rx << ") + String(rx));
-    Serial.println(String("    tx >> ") + String(tx));
+  // wait for a messsage (ascii set in interrupt routine, hex set below)
+  if (msg_ready ) {
+    if (is_ascii) {
+        tx [pos] = 0;  // make sure we're nul terminated
+        rx [pos] = 0;  
+        Serial.print(String("rx << ") + String(rx));
+        Serial.println(String("    tx >> ") + String(tx));
+    }
+    else {
+        Serial.print("rx<< ");
+        for (int ii = 0; ii < pos; ii++) {
+            if (rx[ii] < HEX) 
+                Serial.print("0");
+            Serial.print((unsigned char)rx[ii], HEX);
+            Serial.print(" ");
+        }
+        Serial.println("");
+    }
     pos = 0;
-    process_it = false;
+    msg_ready = false;
   }
 }  
